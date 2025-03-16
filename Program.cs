ec9f7624e1a1794c0c12639ea48d9fce77bde8b1
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-16 19:58:03 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+double valorDolar = 0.0, cotação= 0.0, valorReal = 0.0;
+
+Console.Write("Insira um valor em dolar: ");
+valorDolar= double.Parse(Console.ReadLine());
+
+Console.Write("Insira a cotação atual: ");
+cotação= double.Parse(Console.ReadLine());
+
+valorReal= valorDolar * cotação;
+
+Console.WriteLine($"Você tem: BRL {valorReal.ToString("F2")}");
+
